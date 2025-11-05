bbffbeeb20dbb6050f9d57074728ff27ee6e7a65
LuceNyx <adriana.castro@utec.edu.pe>
2025-11-04 22:18:01 -0500

    Refactor lambda_handler to support event body
    Updated lambda_handler to handle JSON body input.

--- BuscarAlumno.py
+++ BuscarAlumno.py
@@ -1,9 +1,16 @@
 import boto3
+import json
 
 def lambda_handler(event, context):
+    if 'body' in event:
+        body = json.loads(event['body'])
+    else:
+        body = event
+
     # Entrada (json)
-    tenant_id = event['tenant_id']
-    alumno_id = event['alumno_id']
+    tenant_id = body['tenant_id']
+    alumno_id = body['alumno_id']
+
     # Proceso
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('t_alumnos')
@@ -13,6 +20,7 @@ def lambda_handler(event, context):
             'alumno_id': alumno_id
         }
     )
+
     # Salida (json)
     return {
         'statusCode': 200,
